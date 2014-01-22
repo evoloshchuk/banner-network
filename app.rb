a917fe33d1ee7b664bec956926561f5ca457b20d
Ievgen Voloshchuk <evoloshchuk@gmail.com>
2014-01-22 22:37:54 +0100

    fix redis configuration

--- app.rb
+++ app.rb
@@ -6,22 +6,15 @@ require_relative 'controllers/init'
 require_relative 'libs/init'
 require_relative 'jobs/init'
 
-configure :production do
-  @redis_server = ENV["REDISTOGO_URL"]
-end
-
-configure :development do
-  @redis_server = 'http://127.0.0.1:6379'
-end
-
-Redis.current = Redis.new(:url => @redis_server)
+$redis_server = ENV.fetch("REDISTOGO_URL", 'http://127.0.0.1:637911')
+Redis.current = Redis.new(:url => $redis_server)
 
 class Application < Sinatra::Application
 
   enable :logging
 
   configure :production, :development do
-    use Rack::Session::Moneta, :store => :Redis, :server => @redis_server
+    use Rack::Session::Moneta, :store => :Redis, :server => $redis_server
   end
   configure :test do
     use Rack::Session::Moneta, :store => :Memory
